977ee0fdb844b882821eeb9ae50782d4a54e4e84
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-12-01 10:13:49 +0900

    [level 0] Title: 문자열 밀기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,11 @@
 function solution(A, B) {
-    // A 길이만큼 반복해서 확인 -> B랑 일치하면 멈춤
+    // A를 여러 번 밀어보며 B와 같은지 확인
     for (let i = 0; i < A.length; i++){
         if(A === B){
-        return i;
+        return i; // 밀어서 B와 같아지는 최소 횟수 반환
     }
-    A = A.slice(-1) + A.slice(0, -1);
+        // A를 오른쪽으로 한 칸 밀기
+        A = A.slice(-1) + A.slice(0, -1);
     }
-    return -1
+    return -1 // 끝까지 확인해도 같아지지 않으면 -1 반환
 }
