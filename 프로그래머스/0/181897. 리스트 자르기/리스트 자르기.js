7c5cdd391f340aa8e345011d41681bbd1e45cdd3
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-12-12 09:40:57 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,17 @@
+function solution(n, slicer, num_list) {
+    const [a, b, c] = slicer
+    let result = []
+    
+    if(n === 1){
+        return num_list.slice(0, b + 1)
+    }else if(n === 2){
+        return num_list.slice(a)
+    }else if(n === 3){
+        return num_list.slice(a, b + 1)
+    }else if(n === 4){
+        return num_list.slice(a, b + 1).filter((num,index)=>{
+            return index % c === 0
+        })
+    }
+}
+    
