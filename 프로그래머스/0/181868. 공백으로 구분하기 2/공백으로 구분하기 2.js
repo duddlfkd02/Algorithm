8a7e895ea3fdbf66364dd3b80b2d8320f7937550
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-12-17 09:39:43 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,7 +1,5 @@
 function solution(my_string) {
-    let splitArr = my_string.split(" ")
-    
-    return splitArr.filter((str) => {
-        if(str.length !==0) return str
+    return my_string.split(" ").filter((str) => {
+        return str
     })
 }
