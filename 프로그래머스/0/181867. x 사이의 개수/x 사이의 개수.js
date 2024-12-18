22842c10b9d4b91a970b3e58eb3e6716e364e10e
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-12-18 11:03:25 +0900

    [level 0] Title: x 사이의 개수, Time: 3.56 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,9 +1,4 @@
 function solution(myString) {
-    const splitStr = myString.split("x");
-    let result = []
-    
-    for(let i = 0; i < splitStr.length; i++){
-        result.push(splitStr[i].length) 
-    }
-    return result
+    return myString.split("x").map((str) => str.length)
 }
+
