f986b88f699e3d39759e9b5c2ea0b6b49274acdd
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-12-27 12:52:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.81 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr){
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        if (i === 0 || arr[i] !== arr[i - 1]) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
