60662af9805cddccc78fd743296ffda5e1457bbd
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2025-01-05 16:15:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.16 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,19 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
+    const map = new Map();
     
-    for(let i = 0; i < completion.length; i++){
-        if(participant[i] !== completion[i]){
-            return participant[i];
+    // participant map에 저장
+    for(const name of participant){
+        map.set(name, (map.get(name) || 0) + 1);
+    }
+    
+    // completion 이름 map에서 제거
+    for(const name of completion){
+        map.set(name, map.get(name) -1);
+    }
+    
+    for(const [name, count] of map){
+        if(count > 0){
+            return name;
         }
     }
-    return participant[participant.length -1]
 }
