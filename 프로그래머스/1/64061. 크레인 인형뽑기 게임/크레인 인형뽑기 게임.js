16a92ca5e3c88d8cc07c47ba03ee2311fa45408d
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2025-01-07 09:44:26 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 3.73 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,14 +1,9 @@
 function solution(board, moves) {
-    // board 열을 스택처럼 써보기
-    // moves는 1부터 시작 -> 0으로 만들기
-    // pop메서드로 moves를 인덱스로 접근해서 board 마지막 요소 빼기
-    // result 빈 배열에 뺀 요소 넣은 후, 같은 숫자 연달아 있는거 count하기
-    
     const basket = [];
     let count = 0;
     
     for(const move of moves){
-        const col = move - 1; // board pop 할 때 index 로 사용할 것
+        const col = move - 1; 
         for(let i = 0; i < board.length; i++){
             if(board[i][col] !== 0){
                 const selectDoll = board[i][col];
