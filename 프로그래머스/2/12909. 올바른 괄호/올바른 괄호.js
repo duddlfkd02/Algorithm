b47cf49a19c9943f90da1115f5b380590558de5a
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2025-01-15 09:17:28 +0900

    [level 2] Title: 올바른 괄호, Time: 29.32 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s){
+    let count = 0;
+    for(const char of s){
+        if(char === "("){
+            count++;
+        }else{
+            count--;
+        }
+        if(count < 0){ 
+            return false;
+        }
+    }
+    return count === 0;
+}
