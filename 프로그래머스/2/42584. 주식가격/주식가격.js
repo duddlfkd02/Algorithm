241ea9294fba977dee2293e0e4966999075cd7df
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2025-02-05 09:19:04 +0900

    [level 2] Title: 주식가격, Time: 31.14 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,14 +1,20 @@
 function solution(prices) {
-    let result = [];
-    for(let i = 0; i < prices.length; i++){
-        let stack = 0;
-        for(let j = i + 1; j < prices.length; j++){
-            stack++;
-            if(prices[i] > prices[j]){
-                break;
-            }
+    const length = prices.length;
+    const result = new Array(length).fill(0);
+    const stack = []; // 시간, 가격 저장
+    
+    for(let i = 0; i < length; i++){
+        // 현재 가격 < 스택 최상단 가격
+        while(stack.length && prices[i] < prices[stack[stack.length -1]]){
+            const lastIndex = stack.pop();
+            result[lastIndex] = i - lastIndex;
         }
-        result.push(stack);
+        stack.push(i);
+    }
+    // 스택에 남아있는 시간들 처리
+    while(stack.length){
+        const lastIndex = stack.pop();
+        result[lastIndex] = length - 1 - lastIndex;
     }
     return result;
 }
