a7fe0ea90c640842f916267454095ceca9c4f299
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2025-02-09 16:13:34 +0900

    [level 2] Title: 오픈채팅방, Time: 319.88 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,26 +1,22 @@
 function solution(record) {
     const result = [];
-    const userInfo = {};
+    const userInfo = new Map();
     
     // uid 기준으로 닉네임 저장하기
-    for(const element of record){
-        const [state, id, nickname] = element.split(" ");
-        
-        if(state === "Enter" || state === "Change"){
-            userInfo[id] = nickname;
-        }
-    }
+   record.forEach((entry) => {
+       const [state, id, nickname] = entry.split(" ");
+       
+       if(state !== "Leave"){
+           userInfo.set(id, nickname); // 처음 들어왔거나, 닉변 값 업데이트
+       }
+   })
     
-    // ~코멘트 기록
-    for(const element of record){
-        const [state, id] = element.split(" ");
+    // 메시지 기록
+    return record.filter((entry) => entry.startsWith("Enter") || entry.startsWith("Leave"))
+    .map((entry) => {
+        const [state, id] = entry.split(" ");
+        const nickname = userInfo.get(id);
+        return state === "Enter" ? `${nickname}님이 들어왔습니다.` : `${nickname}님이 나갔습니다.`;
         
-        if (state === "Enter") {
-            result.push(`${userInfo[id]}님이 들어왔습니다.`);
-        } else if (state === "Leave") {
-            result.push(`${userInfo[id]}님이 나갔습니다.`);
-        }
-    }
-    
-    return result;
+    })
 }
