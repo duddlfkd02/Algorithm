d73b71ddbfed10c77806ae7221018276a9b82cf8
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2025-03-17 09:53:16 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 59.83 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,32 +1,35 @@
 function solution(diffs, times, limit) {
+    let left = 1;  
+    let right = 100000;
+    let result = right;  
     
-    let max = 100000, min = 1, mid = undefined
-    let answer = max
-    while(min<=max){
-        mid = Math.floor((max+min)/2)
-        let spendTime = 0, over = false
-        for(let i=0; i<diffs.length; ++i){
-            
-            if(mid-diffs[i]<0){
-                spendTime = spendTime + (diffs[i]-mid)*(times[i]+times[i-1]) + times[i] 
-            }else{ 
-                spendTime+= times[i]
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2); 
+        let totalTime = 0;
+        let over = false; // 제한 시간 초과 여부
+        
+        for (let i = 0; i < diffs.length; i++) {
+            if (diffs[i] > mid) { // 숙련도가 부족한 경우
+                let fail = Math.max(diffs[i] - mid, 0);
+                let prevTime = i > 0 ? times[i - 1] : 0; // i = 0일 때 예외 처리
+                totalTime += fail * (times[i] + prevTime) + times[i];
+            } else { // 숙련도로 해결 가능한 경우
+                totalTime += times[i];
             }
-            
-            if(limit<spendTime){
-                over = true
-                break;
+
+            if (totalTime > limit) { 
+                over = true;
+                break; // 제한 시간 초과 시 반복문 종료
             }
         }
-        
-        if(over){
-           min = mid + 1
-        }else{
-           answer = mid 
-           max = mid -1 
+
+        if (over) { 
+            left = mid + 1;
+        }else {
+            result = mid; 
+            right = mid - 1; // 더 작은 숙련도로 가능한지 탐색
         }
-        
     }
-    return answer
-    
+
+    return result;
 }
