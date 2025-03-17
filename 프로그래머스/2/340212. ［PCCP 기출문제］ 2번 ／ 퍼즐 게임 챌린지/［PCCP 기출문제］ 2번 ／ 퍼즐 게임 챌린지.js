51b0053432041e4d5c48ce0e53653e759b3b245c
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2025-03-17 09:54:20 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 48.08 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,17 +1,17 @@
 function solution(diffs, times, limit) {
     let left = 1;  
-    let right = 100000;
+    let right = 100000; 
     let result = right;  
-    
+
     while (left <= right) {
-        let mid = Math.floor((left + right) / 2); 
+        let mid = Math.floor((left + right) / 2);
         let totalTime = 0;
-        let over = false; // 제한 시간 초과 여부
+        let over = false; // 제한 시간 초과 여부 
         
         for (let i = 0; i < diffs.length; i++) {
             if (diffs[i] > mid) { // 숙련도가 부족한 경우
-                let fail = Math.max(diffs[i] - mid, 0);
-                let prevTime = i > 0 ? times[i - 1] : 0; // i = 0일 때 예외 처리
+                let fail = Math.max(diffs[i] - mid, 0); 
+                let prevTime = i > 0 ? times[i - 1] : 0; 
                 totalTime += fail * (times[i] + prevTime) + times[i];
             } else { // 숙련도로 해결 가능한 경우
                 totalTime += times[i];
@@ -19,13 +19,13 @@ function solution(diffs, times, limit) {
 
             if (totalTime > limit) { 
                 over = true;
-                break; // 제한 시간 초과 시 반복문 종료
+                break;
             }
         }
 
         if (over) { 
-            left = mid + 1;
-        }else {
+            left = mid + 1; // limit 초과하면 더 높은 숙련도가 필요
+        } else {
             result = mid; 
             right = mid - 1; // 더 작은 숙련도로 가능한지 탐색
         }
